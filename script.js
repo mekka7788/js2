1abacb475404cc08bc2b3788b97b6d4bc3bcc5b6
mekka7788 <meerimaalisherova@gmail.com>
2023-07-12 10:06:55 +0300

    js-2

---
+++ script.js
@@ -0,0 +1,80 @@
+// /*let a = "Meerim"
+// let b = "Alisherova"
+// let c = a.slice(5)
+// let d = b.substring(11)
+// console.log(a)*/
+// let a = 'hello'
+// let b = 'ivan'
+// console.log(a[0].toUpperCase() + a.slice(1) +' '+
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+// console.log(b)
+// let a = 'lorem ipsum dolor sit amet'
+// let b= a.slice(a.lastIndexOf('s'),a.lastIndexOf('s' ) +3)
+// console.log(b+' '+a.slice(0, a.lastIndexOf('r')+1)+' '+a.slice(a.lastIndexOf('a')))
+// let a = 'children'
+// let b = a.slice( 0,a.lastIndexOf('n')).toUpperCase()
+// console.log(b+a.slice(a.lastIndexOf('n')))
+// let a = 'boss'
+// let b = a.slice(a.indexOf('s'), a.indexOf('s') + 2)
+// console.log(a[0] + b + a[1])
+// let a = 'sunday'
+// console.log(a[3])
+
+// let a = 'user'
+// let b = 'new'
+// // верунть new us
+// console.log(b+' '+a.slice(0,2))
+
+// let a = 'monday'
+// let b = 'day'
+// // вернуть daydaymon
+// console.log(b+b+a.slice(0,3))
+
+//let a = 'HELLO'
+// вернуть 'hElLo'
+// console.log(a[0].toLowerCase()+a[1]+a[2].toLowerCase()+a[3]+a[4].toLowerCase())
+
+// let a = 5
+// let b = '400'
+// // вернуть '450'
+// console.log(b.replace('0',a))
+//
+//  let a = 'monkey'
+// let b = 'banana'
+// // вернуть manak
+// console.log(a[0]+ b.slice(1,4)+a[3])
+//  let a = 'hello'
+// // верунть hekso
+// let b = 'ks'
+// console.log(a.slice(0,2)+b+a.slice(4))
+
+
+
+
+//ваше имя
+//ваш возраст 18 до 50
+//город
+//добро пожаловать имя
+//я васне знаю
+//возраст:возраст долженбыть старше 18и моложе 50
+//это не ваш город
+
+// function checkUser(user,age,city){
+//    if (user==='Misha') {
+//        if (city==='Tokmok') {
+//            if (age > 18 && age < 30) {
+//                return 'You are Misha from Tokmok! You are 18-30 years old! Congratutations'
+//
+//            }
+//        }
+//        return 'Hello,my guest Vlad'
+//    }
+// }
+//
+// console.log(checkUser(user))
+
+
+
+
+
